029d2ad19460a4bd27228700e83372fe9f6fc8c3
Unknown <brendonrichards93@gmail.com>
2018-08-08 10:27:35 +0200

    Setup Pug template engine

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-const express = require("express");
+const express = require("express"),
+      path    = require("path");
 
+// Init App
 const app = express();
 
+// Load View Engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Home Route
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  let articles = [
+    {
+      id: 1,
+      title: "Article One",
+      author: "Brendon Richards",
+      body: "This is article 1."
+    },
+    {
+      id: 2,
+      title: "Article Two",
+      author: "John Doe",
+      body: "This is article 2."
+    },
+    {
+      id: 1,
+      title: "Article Three",
+      author: "Brendon Richards",
+      body: "This is article 3."
+    }
+  ]
+  res.render("index", {
+    title: "Articles",
+    articles: articles
+  });
 });
 
+// Add Route
+app.get("/articles/add", (req, res) => {
+  res.render("add_article", {
+    title: "Add Article"
+  });
+});
+
+// Start Server
 app.listen(3000, () => {
   console.log("Server started on port 3000...");
 });
