41e87c6ca43caede7f3dd4a8e53c2d25c3a6af62
Unknown <brendonrichards93@gmail.com>
2018-08-09 08:24:32 +0200

    Add update and delete article functionality

--- app.js
+++ app.js
@@ -83,6 +83,47 @@ app.post("/articles/add", (req, res) => {
   });
 });
 
+// Load Edit Form
+app.get("/article/edit/:id", (req, res) => {
+  Article.findById(req.params.id, (err, article) => {
+    res.render("edit_article", {
+      title: "Edit Article",
+      article: article
+    });
+  });
+});
+
+// Update Submit POST Route
+app.post("/articles/edit/:id", (req, res) => {
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = { _id:req.params.id }
+
+  Article.update(query, article, (err) => {
+    if (err) {
+      console.log(err);
+      return
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+app.delete("/article/:id", (req, res) => {
+  let query = { _id: req.params.id }
+
+  Article.remove(query, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send("Successfully deleted");
+    }
+  });
+});
+
 // Start Server
 app.listen(3000, () => {
   console.log("Server started on port 3000...");
