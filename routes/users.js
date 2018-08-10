2bb04d2ec3aec7278a12db80bc4cff73c23f1df5
Unknown <brendonrichards93@gmail.com>
2018-08-10 10:32:57 +0200

    Implement user registration

---
+++ routes/users.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+// Bring in User Model
+const User = require("../models/user");
+
+// Register Form
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+//Register Process
+router.post("/register", (req, res) => {
+  const name = req.body.name,
+        email = req.body.email,
+        username = req.body.username,
+        password = req.body.password,
+        password2 = req.body.password2;
+
+  req.checkBody("name", "Name is required").notEmpty();
+  req.checkBody("email", "Email is required").notEmpty();
+  req.checkBody("email", "Email is not valid").isEmail();
+  req.checkBody("username", "Username is required").notEmpty();
+  req.checkBody("password", "Password is required").notEmpty();
+  req.checkBody("password2", "Passwords do not match").equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if (errors) {
+    res.render("register", {
+      errors: errors
+      
+    });
+  } else {
+    let newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) {
+          console.log(err)
+        }
+        newUser.password = hash;
+        newUser.save((err) => {
+          if (err) {
+            console.log(err);
+            return;
+          } else {
+            req.flash("success", "You are now registered and can now log in");
+            res.redirect("/users/login");
+          }
+        });
+      });
+    });
+  }
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+module.exports = router;
